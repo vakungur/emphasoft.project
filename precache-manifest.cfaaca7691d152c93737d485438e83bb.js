684696ebea32acbcf12b5f09934ab97fb653809a
vakungur <vakungur@gmail.com>
2020-08-28 14:35:42 +0300

    Updates

---
+++ precache-manifest.cfaaca7691d152c93737d485438e83bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f88942785fd53332880b60eb54cd8a8",
+    "url": "/emphasoft/index.html"
+  },
+  {
+    "revision": "79db14b6f5047ec6e204",
+    "url": "/emphasoft/static/css/2.fe7cd3c1.chunk.css"
+  },
+  {
+    "revision": "e80762666265f129e2b8",
+    "url": "/emphasoft/static/css/main.3a2413b6.chunk.css"
+  },
+  {
+    "revision": "79db14b6f5047ec6e204",
+    "url": "/emphasoft/static/js/2.23e74226.chunk.js"
+  },
+  {
+    "revision": "513eaf41fd00af7b5315e05327e556e9",
+    "url": "/emphasoft/static/js/2.23e74226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80762666265f129e2b8",
+    "url": "/emphasoft/static/js/main.7b7fc123.chunk.js"
+  },
+  {
+    "revision": "7c57b725d8957e80a18d",
+    "url": "/emphasoft/static/js/runtime-main.81be181a.js"
+  },
+  {
+    "revision": "1610a0ff82fcaf4f3ea5ba61b94dfc29",
+    "url": "/emphasoft/static/media/backgroundAuth.1610a0ff.jpg"
+  }
+]);
